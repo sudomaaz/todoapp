47be815abcc4d4e1f996e4b47859edcf9c744fcd
sudomaaz <maazm0649@gmail.com>
2021-08-02 04:56:58 +0530

    updated knex file

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,23 @@ module.exports = {
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PWD,
+      host: process.env.DB_HOST,
     },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "db/migrations",
+    },
+    seeds: {
+      directory: "db/seeds",
+    },
+  },
+  development: {
+    client: "postgresql",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
