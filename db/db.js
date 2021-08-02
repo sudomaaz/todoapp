7ed855378a595c5bdefcca49f16271f8f2c0ca4f
sudomaaz <maazm0649@gmail.com>
2021-08-02 06:19:21 +0530

    updated database

--- db/db.js
+++ db/db.js
@@ -9,6 +9,9 @@ function setupDB() {
   //console.log(env);
   const knex = Knex(knexfile[env]);
 
+  await knex.migrate.rollback(knexfile[env]);
+  await knex.migrate.latest(knexfile[env]);
+  await knex.seed.run(knexfile[env]);
   // Give the knex instance to objection.
   Model.knex(knex);
 }
